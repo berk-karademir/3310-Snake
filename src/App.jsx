c9130a2a595a2d8d510ab38cae9f3edb0ea24a21
berk-karademir <berk.karademir@hotmail.com>
2025-01-08 21:00:55 +0300

    "work in progress" warning added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import GameBoard from "./GameBoard"
+import GameBoard from "./GameBoard";
 
 function App() {
   return (
-    <div className=" bg-[#f0f0f0] flex items-center justify-center">
+    <div className="bg-[#f0f0f0] flex items-center justify-center">
       <div className="relative">
         {/* Nokia Phone Image */}
         <img
@@ -10,14 +10,17 @@ function App() {
           alt="Nokia 3310"
           className="w-screen h-screen"
         />
-        
+        {/* Heading Over the Image */}
+        <p className="absolute bottom-5 left-[70%] text-white text-2xl font-bold italic">
+          work in progress, not final product.
+        </p>
         {/* Game Screen Overlay */}
         <div className="absolute top-[30%] left-[42.7%] w-[275px] h-[174px] overflow-hidden rounded-md">
           <GameBoard />
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
